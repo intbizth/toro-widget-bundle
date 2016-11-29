129dfa705420819603ce9cfcae1b829d2b441ec5
liverbool <nukboon@gmail.com>
2016-11-30 00:57:50 +0700

    fix compile pass

--- DependencyInjection/Compiler/RegisterWidgetPass.php
+++ DependencyInjection/Compiler/RegisterWidgetPass.php
@@ -33,20 +33,20 @@ class RegisterWidgetPass implements CompilerPassInterface
 
             $reflex = new \ReflectionClass($class);
 
-            if ($reflex->implementsInterface(WidgetInterface::class)) {
-                $definition->addMethodCall('setRouter', [new Reference('router')]);
-
-                if (isset($widgets[$class])) {
-                    $definition->addMethodCall('setDefaultOptions', [$widgets[$class]['options']]);
-                }
-            }
-
             if ($reflex->implementsInterface(ContainerAwareWidgetInterface::class)) {
                 $definition->addMethodCall('setContainer', [new Reference('service_container')]);
             }
 
             if ($reflex->implementsInterface(ChannelAwareWidgetInterface::class)) {
                 $definition->addMethodCall('setChannelContext', [new Reference('sylius.context.channel')]);
+            }
+
+            if ($reflex->implementsInterface(WidgetInterface::class)) {
+                $definition->addMethodCall('setRouter', [new Reference('router')]);
+
+                if (isset($widgets[$class])) {
+                    $definition->addMethodCall('setDefaultOptions', [$widgets[$class]['options']]);
+                }
             }
         }
     }
